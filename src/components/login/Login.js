d8519350d97cffca9f4481d1f1a2e4eaa7ba03e5
Agustinaa01 <agustina04022001@hotmail.com>
2023-09-16 18:15:26 -0300

    Cambios css

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -44,36 +44,33 @@ const LoginForm = () => {
   
 
   return (
-    <div className="login-container">
+    // <div className="login-container">
       <div className="login-box">
-        <h4>Iniciar Sesión</h4>
+        <h2>Iniciar Sesión</h2>
         <div className="input-container">
           <input
-            className="input-control"
             onChange={handleEmailChange}
             placeholder="Email"
             type="email"
             ref={emailRef}
-          />
+          /><br></br>
           <input
-            className="input-control"
             onChange={handlePasswordChange}
             placeholder="Password"
             type="password"
             ref={passwordRef}
           />
-        </div>
+        </div><br></br>
+        <div className="input-button">
         <button
           onClick={handleLoginClick}
           className="signin-button"
           type="button">
           Iniciar sesión
         </button>
+        </div>
       </div>
-      <div className="image-background">
-      {/* Coloca aquí tu imagen de fondo */}
-    </div>
-    </div>
+    // </div>
   );
 }
 
