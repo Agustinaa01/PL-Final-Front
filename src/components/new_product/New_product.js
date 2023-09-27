69910620522bc5830b31c715ba1932c0d357f96e
GentleMadman <lucianopilot19@gmail.com>
2023-09-27 17:48:59 -0300

    agregado archivo css de New Product

--- src/components/new_product/New_product.js
+++ src/components/new_product/New_product.js
@@ -1,5 +1,6 @@
-import { useRef, useState } from "react";
-import { useNavigate } from "react-router";
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./New_product.css";
 
 const ProductForm = () => {
   // estados para nombre,precio,color,stock y descripcion
@@ -25,23 +26,82 @@ const ProductForm = () => {
   const handlePriceChange = (event) => {
     priceRef.current.style.bordercolor = "";
     priceRef.current.style.outline = "";
-    setName(event.target.value);
+    setPrice(event.target.value);
   };
   const handleColorChange = (event) => {
     colorRef.current.style.bordercolor = "";
     colorRef.current.style.outline = "";
-    setName(event.target.value);
+    setColor(event.target.value);
   };
   const handleStockChange = (event) => {
     stockRef.current.style.bordercolor = "";
     stockRef.current.style.outline = "";
-    setName(event.target.value);
+    setStock(event.target.value);
   };
   const handleDescChange = (event) => {
     descRef.current.style.bordercolor = "";
     descRef.current.style.outline = "";
-    setName(event.target.value);
+    setDesc(event.target.value);
   };
+  // Alerta de campos vacios
+  const handleAddClick = () => {
+    if (
+      nameRef.current.value.length === 0 ||
+      priceRef.current.value.length === 0 ||
+      colorRef.current.value.length ||
+      stockRef.current.length === 0 ||
+      descRef.current.length === 0
+    ) {
+      alert("Completa todos los campos");
+      return;
+    }
+  };
+
+  return (
+    <div className="name">
+      <div className="name-box">
+        <h2>Nombre</h2>
+        <div className="container">
+          <input
+            onChange={handleNameChange}
+            placeholder="Nombre"
+            type="nombre"
+            ref={nameRef}
+          />
+          <input
+            onChange={handlePriceChange}
+            placeholder="Precio"
+            type="number"
+            ref={priceRef}
+          />
+          <input
+            onChange={handleStockChange}
+            placeholder="Stock"
+            type="number"
+            ref={stockRef}
+          />
+          <input
+            onChange={handleColorChange}
+            placeholder="Color"
+            type="name"
+            ref={colorRef}
+          />
+          <input
+            onChange={handleDescChange}
+            placeholder="Descripcion"
+            type="descripcion"
+            ref={descRef}
+          />
+        </div>
+        <div className="add-button">
+          <button>
+            onClick={handleAddClick}
+            type="button" Añadir producto
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductForm;
