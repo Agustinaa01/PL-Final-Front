f4d6e1b542b7943b4d8112583f6bf76178bf79a9
Agustinaa01 <112518086+Agustinaa01@users.noreply.github.com>
2023-10-16 22:04:17 -0300

    product details

--- src/components/productDetails/productDetails.js
+++ src/components/productDetails/productDetails.js
@@ -11,16 +11,20 @@ const ProductDetailsForm = () => {
             <div className="product-details-container">
                 <img className="imagen-details" src={foroDetailsImage} alt="Product Details" />
             <div className="product-info">
-                <h2>Parlante</h2><br />
-                <h5>Price: $99.99</h5><br />
-                <p>Category: Electronics</p>
-                <button>Agregar producto</button><br/><br/>
-                <button>Editar producto</button><br/><br/>
-                <button>Eliminar producto</button> 
+                <h2>Cargador Wireless Negro</h2><br />
+                <h5>Precio: $20.840</h5><br />
+                <p>Categoria: Cargador</p>
+                <p>Marca: Samsung</p>
+                <button className="button-details">Agregar al carrito</button><br/>
+                <button className="button-details">Editar producto</button><br/>
+                <button className="button-details-delete">Eliminar producto</button> 
+            </div>
             </div>
             <div className="description">
-                <p></p>
-            </div>
+            <h2>Descripcion</h2>
+            <p>
+                Este cargador inalámbrico negro de Samsung es la solución perfecta para mantener tus dispositivos cargados de manera conveniente y elegante. Con un diseño elegante y una compatibilidad amplia, este cargador es ideal para tu estilo de vida en constante movimiento. Carga tus dispositivos de forma rápida y segura con la tecnología de carga inalámbrica líder en la industria.
+            </p>
             </div>
         </div>
     );
