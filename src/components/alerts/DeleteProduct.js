d0bb1dc4fd50f68627b7c3e0025e86a7fbe6e735
GentleMadman <lucianopilot19@gmail.com>
2023-10-24 14:19:47 -0300

    Agregado DeleteProduct

--- src/components/alerts/DeleteProduct.js
+++ src/components/alerts/DeleteProduct.js
@@ -1,25 +1,29 @@
 import { useNavigate } from "react-router";
 
-function alert = () => {
+const alert = () => {
   const DeleteProduct = () => {
     const navigate = useNavigate();
 
     const handleConfirmClick = () => {
-      navigate();
+      navigate("/products");
     };
 
     const handleCancelClick = () => {
-      navigate();
+      navigate("/products");
     };
   };
 
-  return(
-    <div>className='Warning'
+  return (
+    <div>
+      className='Warning'
       <h1 className="title">ADVERTENCIA</h1>
       <h2 className="text">Esta seguro que quiere eliminar el producto?</h2>
-      <button className="Confirm" onClick={handleConfirmClick}>Confirmar</button>
-      <button className="Cancel" onClick={handleCancelClick}>Cancelar</button>
-      </div>
-
-  )
+      <button className="Confirm" onClick={handleConfirmClick}>
+        Confirmar
+      </button>
+      <button className="Cancel" onClick={handleCancelClick}>
+        Cancelar
+      </button>
+    </div>
+  );
 };
