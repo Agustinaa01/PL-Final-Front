4dccb3283b707e3c076c834716eb6c0ca9e663c5
Agustinaa01 <112518086+Agustinaa01@users.noreply.github.com>
2023-11-09 16:17:55 -0300

    fixes toastify

--- src/components/productDetails/productDetails.js
+++ src/components/productDetails/productDetails.js
@@ -4,10 +4,10 @@ import Headers from "../header/Headers";
 import { useContext, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { Modal, ModalHeader } from "react-bootstrap";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { AuthenticationContext } from "../services/authentication/AuthenticationContext";
-import { ThemeContext } from "../services/theme/ThemeContext"; 
+import { ThemeContext } from "../services/theme/ThemeContext";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductDetailsForm = () => {
   const location = useLocation();
@@ -17,7 +17,6 @@ const ProductDetailsForm = () => {
   const [price, setPrice] = useState(location.state?.productSelected?.price);
   const [brand, setBrand] = useState(location.state?.productSelected?.brand);
   const [showPaymentModal, setShowPaymentModal] = useState(false);
-
   const [category, setCategory] = useState(
     location.state?.productSelected?.category
   );
@@ -92,7 +91,7 @@ const ProductDetailsForm = () => {
         <Headers />
       </div>
       <div className="product-details-container">
-        <img className= {`${ClassDetails}`}src={image} alt="Product Details" />
+        <img className={`${ClassDetails}`} src={image} alt="Product Details" />
         <div className="producto-info">
           <h1>{name}</h1>
           <br />
@@ -116,14 +115,22 @@ const ProductDetailsForm = () => {
               <Modal.Title>Confirmación</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              ¿Desea agregar este producto al carrito? 
-              <p><strong>Debe iniciar sesión para continuar.</strong></p>
+              ¿Desea agregar este producto al carrito?
+              <p>
+                <strong>Debe iniciar sesión para continuar.</strong>
+              </p>
             </Modal.Body>
             <Modal.Footer>
-              <button className="button-cancel" onClick={closeConfirmationModal}>
+              <button
+                className="button-cancel"
+                onClick={closeConfirmationModal}
+              >
                 Cancelar
               </button>
-              <button className="button-confirm" onClick={() => navigate("/login")}>
+              <button
+                className="button-confirm"
+                onClick={() => navigate("/login")}
+              >
                 Iniciar sesión
               </button>
             </Modal.Footer>
@@ -134,7 +141,7 @@ const ProductDetailsForm = () => {
               <Modal.Title>ADVERTENCIA</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              Está seguro que quiere eliminar el producto?
+              ¿Está seguro que quiere eliminar el producto?
             </Modal.Body>
             <Modal.Footer>
               <button className="button-cancel" onClick={handleClose}>
@@ -164,7 +171,10 @@ const ProductDetailsForm = () => {
               <p className="total">Total: ${price}</p>
             </Modal.Body>
             <Modal.Footer>
-              <button className="button-cancel" onClick={handleShowPaymentModal}>
+              <button
+                className="button-cancel"
+                onClick={handleShowPaymentModal}
+              >
                 Pagar
               </button>
               <button
@@ -176,56 +186,75 @@ const ProductDetailsForm = () => {
             </Modal.Footer>
           </Modal>
           <Modal show={showPaymentModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Seleccionar Método de Pago</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Seleccione su método de pago preferido:</p>
-          <label>            
-            <input type="radio" name="paymentMethod" value="paypal" />
-            <img  className="img-pay" src="https://cdn-icons-png.flaticon.com/512/147/147258.png"></img>
-            Tarjeta de Credito
-          </label>
-          <br/>
-          <label>            
-            <input type="radio" name="paymentMethod" value="paypal" />
-            <img  className="img-pay" src="https://cdn-icons-png.flaticon.com/512/301/301657.png"></img>
-            Tarjeta de Debito
-          </label>
-          <br/>
-          <label>            
-            <input type="radio" name="paymentMethod" value="paypal" />
-            <img  className="img-pay" src="https://logospng.org/download/mercado-pago/logo-mercado-pago-icone-1024.png"></img>
-            Mercado Pago
-          </label>
-          <br/>
-          <label>            
-            <input type="radio" name="paymentMethod" value="paypal" />
-            <img  className="img-pay" src="https://png.pngtree.com/png-vector/20220821/ourmid/pngtree-bank-transfer-icon-house-selected-transfer-vector-png-image_19626578.png"></img>
-            Transferencia bancaria
-          </label>
-          <br/>
-          <label>            
-            <input type="radio" name="paymentMethod" value="paypal" />
-            <img  className="img-pay" src="https://cdn-icons-png.flaticon.com/512/174/174861.png"></img>
-            PayPal
-          </label>
-          <br/>
-          <label>            
-            <input type="radio" name="paymentMethod" value="paypal" />
-            <img  className="img-pay" src="https://cdn-icons-png.flaticon.com/512/5968/5968279.png"></img>
-            Apple Pay
-          </label>
-          </Modal.Body>
-        <Modal.Footer>
-          <button className="button-cancel" onClick={handleClosePaymentMethodsModal}>
-            Cancelar
-          </button>
-          <button className="button-confirm">
-            Continuar
-          </button>
-        </Modal.Footer>
-      </Modal>
+            <Modal.Header closeButton>
+              <Modal.Title>Seleccionar Método de Pago</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Seleccione su método de pago preferido:</p>
+              <label>
+                <input type="radio" name="paymentMethod" value="paypal" />
+                <img
+                  className="img-pay"
+                  src="https://cdn-icons-png.flaticon.com/512/147/147258.png"
+                ></img>
+                Tarjeta de Credito
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="paymentMethod" value="paypal" />
+                <img
+                  className="img-pay"
+                  src="https://cdn-icons-png.flaticon.com/512/301/301657.png"
+                ></img>
+                Tarjeta de Debito
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="paymentMethod" value="paypal" />
+                <img
+                  className="img-pay"
+                  src="https://logospng.org/download/mercado-pago/logo-mercado-pago-icone-1024.png"
+                ></img>
+                Mercado Pago
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="paymentMethod" value="paypal" />
+                <img
+                  className="img-pay"
+                  src="https://png.pngtree.com/png-vector/20220821/ourmid/pngtree-bank-transfer-icon-house-selected-transfer-vector-png-image_19626578.png"
+                ></img>
+                Transferencia bancaria
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="paymentMethod" value="paypal" />
+                <img
+                  className="img-pay"
+                  src="https://cdn-icons-png.flaticon.com/512/174/174861.png"
+                ></img>
+                PayPal
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="paymentMethod" value="paypal" />
+                <img
+                  className="img-pay"
+                  src="https://cdn-icons-png.flaticon.com/512/5968/5968279.png"
+                ></img>
+                Apple Pay
+              </label>
+            </Modal.Body>
+            <Modal.Footer>
+              <button
+                className="button-cancel"
+                onClick={handleClosePaymentMethodsModal}
+              >
+                Cancelar
+              </button>
+              <button className="button-confirm">Continuar</button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
       <div className={`${textClass}`}>
